3ad85ea1d527cb7eabf83cde16f93e28a62767ab
James Harvey <james.harvey97@live.com>
2026-03-07 17:48:18 +0000

    Initial commit

---
+++ Assets/Scripts/Core/DI/MobaLifetimeScope.cs
@@ -0,0 +1,14 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace DI
+{
+    public class MobaLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}
+
