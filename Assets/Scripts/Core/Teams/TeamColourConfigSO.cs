c8e18e9240a0574646fccfe76eccee89440a4054
James Harvey <james@dwel.ai>
2026-03-08 16:19:23 +0000

    refactor: team colour config

--- Assets/Scripts/Core/Teams/TeamColourConfigSO.cs
+++ Assets/Scripts/Core/Teams/TeamColourConfigSO.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Core.Teams
@@ -5,10 +6,27 @@ namespace Core.Teams
     [CreateAssetMenu(fileName = "NewTeamColourConfig", menuName = "MOBA/Team Colour Config")]
     public class TeamColourConfigSO : ScriptableObject
     {
+        private static readonly int BaseColorProp = Shader.PropertyToID("_BaseColor");
+
         public Color RedTeamColour = Color.red;
         public Color BlueTeamColour = Color.blue;
-        
-        public Color GetColourForTeam(Team team)
+
+        private readonly Dictionary<(Material, Team), Material> _cache = new();
+
+        public Material GetTeamMaterial(Material baseMaterial, Team team)
+        {
+            var key = (baseMaterial, team);
+
+            if (_cache.TryGetValue(key, out var cached))
+                return cached;
+
+            var instance = new Material(baseMaterial);
+            instance.SetColor(BaseColorProp, GetColourForTeam(team));
+            _cache[key] = instance;
+            return instance;
+        }
+
+        private Color GetColourForTeam(Team team)
         {
             return team switch
             {
--- Assets/Scripts/Core/Teams/TeamVisiual.cs
+++ Assets/Scripts/Core/Teams/TeamVisiual.cs
@@ -4,33 +4,24 @@ using VContainer;
 
 public class TeamVisual : MonoBehaviour
 {
-    private static readonly int ColorProp = Shader.PropertyToID("_BaseColor");
-    
     private Renderer _renderer;
-    private MaterialPropertyBlock _propBlock;
+    private Material _baseMaterial;
     private TeamColourConfigSO _teamColourConfig;
-    
+
     [Inject]
     private void Construct(TeamColourConfigSO teamColourConfig)
     {
         _teamColourConfig = teamColourConfig;
     }
-    
+
     private void Awake()
     {
-        _propBlock = new MaterialPropertyBlock();
         _renderer = GetComponent<Renderer>();
+        _baseMaterial = _renderer.sharedMaterial;
     }
 
     public void SetTeam(Team team)
     {
-        _propBlock.SetColor(ColorProp, _teamColourConfig.GetColourForTeam(team));
-        
-        _renderer.SetPropertyBlock(_propBlock);
-    }
-    
-    public void Reset()
-    {
-        _renderer.SetPropertyBlock(null);
+        _renderer.sharedMaterial = _teamColourConfig.GetTeamMaterial(_baseMaterial, team);
     }
 }
